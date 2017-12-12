5abaa4daf8f3bc9ec4273dfcdd9ca94017ed4615
Tatsuki Ishigaki <e165733@ie.u-ryukyu.ac.jp>
2017-12-12 13:19:47 +0900

    Initial Commit

---
+++ Share_Chance/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Share_Chance
+//
+//  Created by 石垣達樹 on 2017/12/12.
+//  Copyright © 2017年 ishitatsu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
